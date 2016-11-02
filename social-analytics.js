b33864e530264c2ea28232b522d1d63b3f7ca2a1
Eric Fredine <eric.fredine@gmail.com>
2016-11-02 23:22:26 +0000

    List all users, who they follow and who they are followed by.

--- social-analytics.js
+++ social-analytics.js
@@ -1,3 +1,4 @@
+"use strict";
 var _ = require('underscore');
 var data =
   {"f01": {name: "Alice",
@@ -19,6 +20,50 @@ var data =
            age: 25,
            follows: ["f05"]}}
 
-_.forEach(data, x => {
-  console.log(x.name);
+var followedBy = _.reduce(data, (followedBy, user, userId) => {
+  user.follows.forEach(followsId => {
+      if(!followedBy[followsId]) {
+        followedBy[followsId] = [];
+      }
+      followedBy[followsId].push(userId);
+    });
+    return followedBy;
+  }, {});
+
+_.forEach(followedBy, (followers, userId) => {
+  data[userId]["followedBy"] = followers;
 });
+
+function getFollowsList(user) {
+  if(user && user.follows && user.follows.length > 0) {
+    return user.follows.map(userId => data[userId]).filter(user => user);
+  }
+  else {
+    return null;
+  }
+}
+
+function userForId(userId) {
+  return data[userId];
+}
+
+function userListString(userIds) {
+  var names = userIds.map(userForId).map(a => a.name);
+  return _.zip(userIds, names).map(t => `${t[1]}(${t[0]})`).join(", ");
+}
+
+function printUser(user, userId) {
+  if(user) {
+    console.log(`${user.name}(${userId}) ---------------`);
+    let follows = userListString(user.follows);
+    let followedBy = userListString(user.followedBy);
+    console.log(`Follows: ${follows}`);
+    console.log(`Followed by: ${followedBy}`);
+  }
+}
+
+function printUsers() {
+  _.forEach(data, (user, userId) => printUser(user, userId));
+}
+
+printUsers();
